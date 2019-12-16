c25f6630e1de603ce18dab450ed62859ca8b5d19
Claus Herther <claus@calogica.com>
2019-12-16 10:39:07 -0800

    Dedupes plays

--- models/analysis/plays.sql
+++ models/analysis/plays.sql
@@ -11,9 +11,27 @@ with plays as (
     where 
         game_date >= cast({{ incremental_refresh_date() }} as date)
     {% endif %}
+),
+numbered_plays as (
+
+    select
+        p.*,
+        row_number() over(partition by game_id, play_id order by (total_home_score+total_away_score) desc) as play_dedupe_sequence_nbr 
+    from 
+        plays p
+
+),
+deduped_plays as (
+    select 
+        *
+    from
+        numbered_plays
+    where
+        play_dedupe_sequence_nbr = 1
 )
 select 
+    {{ dbt_utils.surrogate_key('r.game_id', 'r.play_id' )}} as play_key,
     r.*,
     {{ dbt_housekeeping() }} 
 from 
-    plays r
+    deduped_plays r
