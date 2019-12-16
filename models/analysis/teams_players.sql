1ffa72ccdf5c3c745599fbe44d3937a83db46bc0
Claus Herther <claus@calogica.com>
2019-12-16 10:39:18 -0800

    Updates player key

--- models/analysis/teams_players.sql
+++ models/analysis/teams_players.sql
@@ -7,7 +7,7 @@ with rosters as (
     select * from {{ ref('stg_rosters') }}
 )
 select
-    {{ dbt_utils.surrogate_key('p.season_nbr', 'p.team_code', 'p.player_id' )}} as player_key,
+    {{ dbt_utils.surrogate_key('p.season_nbr', 'p.season_type_code', 'p.team_code', 'p.player_id' )}} as player_key,
     p.*,
     {{ dbt_housekeeping() }}
 from
