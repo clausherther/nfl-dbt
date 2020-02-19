cd16cca185a278b8f1adadc40dc4e1e82fbe274c
Claus Herther <claus@calogica.com>
2020-02-18 20:53:08 -0800

    Add WAP test

--- macros/admin/get_custom_schema.sql
+++ macros/admin/get_custom_schema.sql
@@ -1,6 +1,8 @@
 {% macro generate_schema_name_for_env(custom_schema_name=none) -%}
-    {%- set default_schema = target.schema -%}
-    {%- if custom_schema_name is not none -%}
+    {%- set default_schema = target.schema -%}  
+    {%- if "audit" in target.name -%}
+        audit
+    {%- elif custom_schema_name is not none -%}
         {{ custom_schema_name | trim }}
     {%- else -%}
         {{ default_schema }}
