1fbd9d2c447fa37972917308b72e9e1dc8df50c7
Claus Herther <claus@calogica.com>
2020-02-22 15:48:34 -0800

    Add check for audit flag to test WAP

--- macros/admin/get_custom_schema.sql
+++ macros/admin/get_custom_schema.sql
@@ -1,9 +1,12 @@
 {% macro generate_schema_name_for_env(custom_schema_name=none) -%}
-    {%- set default_schema = target.schema -%}  
-    {%- if "audit" in target.name -%}
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is not none -%}
+        {%- if custom_schema_name not in ("staging") and 
+                ("audit" in target.name or var("audit") == true) -%}
         audit
-    {%- elif custom_schema_name is not none -%}
+        {%- else -%}
         {{ custom_schema_name | trim }}
+        {%- endif -%}
     {%- else -%}
         {{ default_schema }}
     {%- endif -%}
